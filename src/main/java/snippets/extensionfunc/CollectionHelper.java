0ad065fdcc69d238977a583123281257b1907e2a
Jonathan Cornaz <jonathan.cornaz@optravis.com>
2017-10-13 09:39:24 +0200

    Add comparison examples

--- src/main/java/snippets/extensionfunc/CollectionHelper.java
+++ src/main/java/snippets/extensionfunc/CollectionHelper.java
@@ -4,21 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CollectionHelper {
-
-  private CollectionHelper() {}
-
   public static <E> List<E> intersect(List<E> list1, List<E> list2) {
-    HashSet set = new HashSet(list1);
-    List<E> result = new ArrayList<E>(); // here this is the first list
+    Set<E> set = new HashSet<>(list1);
     return list2.stream()
         .filter(set::contains)
         .collect(Collectors.<E>toList());
-  }
-
-  public static void main(String... args) {
-    List<Object> result = CollectionHelper.intersect(Arrays.asList(1, 2, 3), Arrays.asList(2, 3, 4));
   }
 }
